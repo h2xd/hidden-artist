32484af14ae0b0994e2650b019d3f85d356a302b
Andrej Schujkow <a.schujkow@gmail.com>
2024-10-06 15:30:05 +0200

    chore: install biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { ThemeProvider } from "./components/theme-provider";
+import { ThemeModeToggle } from "./components/theme-mode-toggle";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <Button variant="destructive">Click me</Button>
-    </div>
-  )
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<ThemeModeToggle />
+			<div>
+				<Button variant="destructive">Click me</Button>
+			</div>
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
